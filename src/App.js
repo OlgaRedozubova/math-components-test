04156fab24f77590e9ffe5623f33e283ab5d073a
Olga Redozubova <olgaredozubova@redozubova_o_mac_wifi.lan>
2019-03-26 08:17:48 +0200

    home

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
-          MathpixText: <MathpixText text="This is NEW TEXT"/>
+        <div id="preview">
+          MathpixText: <MathpixText text={` ## Mathematics
+When $(a \\ne 0)$, there are two solutions to \\\\(ax^2 + bx + c = 0\\\\) and they are
+    $$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$
+
+
+\\\\[
+\\frac{\\mathrm{d} z_{i}^{2}}{2}\\left[w_{+}^{(i)}(\\boldsymbol{z})+w_{-}^{(i)}(\\boldsymbol{z})\\right]
+\\\\]`}/>
         </div>
       </div>
     );
