9fae7164c0626b36bc3f16252c933cc80af35cbf
Olga Redozubova <olgaredozubova@redozubova_o_mac_wifi.lan>
2019-04-01 14:19:41 +0300

    teting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,113 @@
 import React, { Component } from 'react';
-import MathpixText from 'math-components';
+import {MathpixText, MathpixLoader} from 'react-mathpix';
+import './style.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div id="preview">
-          MathpixText: <MathpixText text={` ## Mathematics
-When $(a \\ne 0)$, there are two solutions to \\\\(ax^2 + bx + c = 0\\\\) and they are
-    $$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$
+      <MathpixLoader>
+        {/*<MathpixText text="\\(ax^2 + bx + c = 0\\)"/>*/}
+        {/*<MathpixText text="(a) Show that $E$ is closed if and only if $\overline { E } = E.$ Show that $E$ is open if and*/}
+{/*only if $E ^ { \circ } = E .$*/}
+{/*(b) Show that $\overline { E } ^ { c } = ( E ^ { c } ) ^ { \circ },$ and similarly that $( E ^ { \circ } ) ^ { c } = \overline { E ^ { c } } .$"/>*/}
+
+        {/*<MathpixText text="Suppose that \(a_{n} \rightarrow a \in \overline{\mathbb{R}}\) . Prove that \[\lim _{n}(a_{1}+\cdots+a_{n}) / n=a\] Is the converse true?"/>*/}
+
+        {/*<MathpixText text=" \[ { Z [ A _ { + } , \eta , \overline { \eta } ] = \int D \overline { \psi } D \psi \exp \{ i \int d ^ { 2 } x [ \overline { \psi } i \gamma ^ { \mu } \partial _ { \mu } \psi + \psi _ { - } ^ { \dagger } A _ { + } \psi _ { - } + \eta \psi _ { + } ^ { \dagger } \psi _ { - } + \overline { \eta } \psi _ { - } ^ { \dagger } \psi _ { + } ] \} }\] \[\left. \begin{aligned}\mathcal { P } _ { + } & = | \langle + | \psi \rangle | ^ { 2 } \\ & = \langle + | \psi \rangle ^ { * } \langle + | \psi \rangle \\ & = \langle \psi | + \rangle \langle + | \psi \rangle \\ & = \langle \psi | P _ { + } | \psi \rangle \end{aligned} \right. \]"/>*/}
+
+        {/*<MathpixText text=" $\begin{aligned} \dot{V}_{k} \leqslant &-\frac{n-k+2}{a} \sum_{i=1}^{k-1} z_{i}^{2}+\left(a \tilde{\Theta}-\sum_{i=1}^{k-1} \frac{\partial W_{i}}{\partial \hat{\Theta}}\right)\left(\tau_{k-1}-\tau\right) \\ &+\frac{3}{2} \overline{\theta} \mu_{k-1}\left(x_{[k]}\right)\left|z_{k-1}\right|^{\frac{2 p_{1} \cdots p_{k-2} - 1}{p_{1} \cdots p_{k-2}}}\left|x_{k}^{p_{k-1}}-\alpha_{k-1}^{p_{k-1}}\right|+\tau \frac{\partial W_{k}}{\partial \hat{\Theta}} \\ &+g_{k} z_{k}^\frac{2p_{1} \cdots p_{k-1}-1}{p_1 \cdots p_{k-1}} x^{p_k}_{k+1} + z_k^\frac{2p_1 \cdots p_{k-1} - 1}{p_1 \cdots p_{k-1}} f_k + \sum_{i=1}^{k-1}\frac{\partial W_k}{\partial x_i}(g_ix^{p_i}_{i+1} + f_i) \end{aligned}$"/>*/}
+
+        {/*<MathpixText text="1. $\mathbf { u } \cdot \mathbf { v } = \overline{ \mathbf { v } \cdot \mathbf { u }}$*/}
+{/*2. $( \mathbf { u } + \mathbf { v } ) \cdot \mathbf { w } = \mathbf { u } \cdot \mathbf { w } + \mathbf { v } \cdot \mathbf { w }$*/}
+{/*3. $( k \mathbf { u } ) \cdot \mathbf { v } = k ( \mathbf { u } \cdot \mathbf { v } )$*/}
+{/*4. $\mathbf { u } \cdot ( k \mathbf { v } ) = \overline { k } ( \mathbf { u } \cdot \mathbf { v } )$"/>*/}
+
+        {/*<MathpixText text=" \[ P [ - t \leq \frac { \overline { X } _ { n } - \mu } { \hat { \sigma } _ { n } / \sqrt { n } } \leq t ] = P [ \overline { X } _ { n } - \frac { t \hat { \sigma } _ { n } } { \sqrt { n } } \leq \mu \leq \overline { X } _ { n } + \frac {t \hat { \sigma } _ { n } } { \sqrt { n } } ] \]"/>*/}
+
+        {/*<MathpixText text="$+\frac{1}{2} \overline{\theta}\left(1+\gamma_{s}\right) \theta \cdot \overline{\varepsilon} \gamma^{\mu}(1+\gamma_s) A_{\mu} \psi(y)$"/>*/}
+
+        {/*<MathpixText text=" $\left.\begin{array}{ll} \overline{A} : & a_{i j}=\int_{\Omega} \nabla \phi_{i} \cdot \nabla \phi_{j} d V \\ \overline{B} : & b_{i j}=\int_{\Omega} \phi_{i} \phi_{j} d V \end{array}\right\} \quad i, j \in[0, \ldots, N]^{2}$"/>*/}
+
+        {/*<MathpixText text=" \[ \overline { A } \frac { \partial ^ { 2} u } { \partial \zeta ^ { 2} } + 2\overline { B } \frac { \partial ^ { 2} \zeta } { \partial \zeta \partial \eta } + \overline { C } \frac { \partial ^ { 2} u } { \partial \eta ^ { 2} } + \Phi ( \zeta \eta, u ,\frac { \partial u } { \partial \zeta } ,\frac { \partial u } { \partial \eta } ) = 0 \]"/>*/}
+
+        {/*<MathpixText text=" $\begin{aligned} \dot{\hat{x}}_{1}(t) &=-\frac{1}{\tau_{1}} \hat{x}_{1}(t)-\frac{\pi}{\nu \overline{P} \sin \left(\frac{n \pi}{L} x_{m}\right)} y(t) \\ \dot{\hat{x}}_{3}(t) &=-\frac{1}{\tau_{3}} \hat{x}_{3}-1-\sqrt{\left|1+\frac{\hat{x}_{1}}{\sqrt{2}}\right|} \end{aligned}$"/>*/}
+
+        {/*<MathpixText text=" $\overline{x}_{\text { Annual lncome, No }}= \frac{125+100+70+120+60+220+75}{7}=110$*/}
+{/*$\begin{aligned} \sigma_{\text { Annual Income, No }}^{2}= &\frac{1}{6}\left[(125-110)^{2}+(100-110)^{2}+(70-110)^{2}+(120-110)^{2}+\right.\\ &(60-110)^{2}+(220-110)^{2}+(75-110)^{2} ]=2975 \end{aligned}$"/>*/}
+
+        {/*<MathpixText text=" $\begin{aligned}\|x+y\|^{2} &=\langle x+y, x+y\rangle \\ &=\langle x, x\rangle+\langle x, y\rangle+\langle y, x\rangle+\langle y, y\rangle \\ &=\|x\|^{2}+\langle x, y\rangle+\overline{\langle x, y\rangle}+\|y\|^{2} \\ &=\|x\|^{2}+2 \operatorname{Re}(\langle x, y\rangle)+\|y\|^{2} \end{aligned}$"/>*/}
+
+        {/*<MathpixText text=" \[ S = A \cap B \cup C \cap \overline { B } \cup A \]"/>*/}
+
+        {/*<MathpixText text=" (ii) $\overline { \mathrm { PQ } }$"/>*/}
+
+        {/*<MathpixText text=" (d) Let $\theta$ be acute angle between the lines*/}
+{/*\[\begin{aligned} \tan \theta &= \frac { 2 \sqrt { h ^ { 2 } - a b } } { ( a + b ) } = \left| \frac { 2 \sqrt { 25 - 24 } } { 14 } \right| = \left| \frac { 2 } { 14 } \right| \\ \tan \theta & = \left| \frac { 1 } { 7 } \right| = 0.\overline{1428571} \in \left( 0,15 ^ { \circ } \right) \end{aligned}\]"/>*/}
+
+        {/*<MathpixText text="\[ \widehat { \mathbf { x } } = \mathbf { x } _ { 0} + \overline { Q } _ { 0} H ^ { T } R ^ { - 1} ( \mathbf { y } - H \mathbf { x } _ { 0} ) \]"/>*/}
+
+        {/*<MathpixText text=" $\begin{aligned}\left(\overline{x}-z_{\frac{\alpha}{2}} \frac{\sigma}{\sqrt{n}}, \overline{x}+z_{\frac{\alpha}{2}} \frac{\sigma}{\sqrt{n}}\right) &=\left(59,87-1.65 \cdot \frac{6}{\sqrt{8}}, 59,87+1.65 \cdot \frac{6}{\sqrt{8}}\right) \\ & \approx(59,87-3,500,58,87+3,500)=(56.37,63.37) \end{aligned}$"/>*/}
+
+        {/*<MathpixText text=" \[ \left. \begin{aligned} \left[ P _ { \mu } , Q _ { \alpha } ^ { I } \right] & = C _ { J } ^ { I } \sigma _ { \mu \alpha \dot { \beta } } \overline { Q } ^ { J \dot { \beta } } \\ [ P _ { \mu } , \overline { Q } _ { \dot { \alpha } } ^ { I } ] & = ( C _ { J } ^ { I } ) ^ { * } \overline { \sigma } _ { \mu \dot { \alpha } \beta } Q ^ { J \beta } \end{aligned} \right. \]"/>*/}
+
+        {/*<MathpixText text=" \[ \Delta _{\varepsilon ,\pm 1,\overline{\varepsilon }}=\frac{\sigma^{2}}{2}\cdot \frac{m-i\varepsilon \widehat{p}}{2m}\cdot\frac{m-i\overline{\varepsilon p}}{2m}\cdot \frac{1}{2}\sigma _{p} ( \sigma _{p}\pm 1) =\Psi _{\varepsilon ,\pm 1,\overline{\varepsilon }}\cdot \overline{\Psi } _{\varepsilon,\pm 1,\overline{\varepsilon }} \]"/>*/}
+
+        {/*<MathpixText text=" $\begin{aligned} f_{\vartheta|\boldsymbol{x}}(\theta) &\propto \frac{\exp \left(-\left(\theta-\mu_{0}\right)^{2} /\left(2 \sigma_{0}^{2}\right)\right)}{\sigma_{0} \sqrt{2 \pi}} \prod_{i=1}^{n} \frac{\exp \left(-\left(x_{i}-\theta\right)^{2} /\left(2 \sigma_{x}^{2}\right)\right)}{\sigma_{x} \sqrt{2 \pi}} \\ &=\frac{1}{(2 \pi)^{(n+1) / 2} \sigma_{0} \sigma_{x}^{n}} \exp \left(-\frac{\left(\theta-\mu_{0}\right)^{2}}{2 \sigma_{0}^{2}}-\frac{\sum_{i=1}^{n}\left(\theta-x_{i}\right)^{2}}{2 \sigma_{x}^{2}}\right) \\ & \propto \exp \left(-\frac{\theta^{2}-2 \mu_{0} \theta}{2 \sigma_{0}^{2}}-\frac{\theta^{2}-2 \overline{x} \theta}{2 \sigma_{x}^{2} / n}\right) \end{aligned}$"/>*/}
+
+          <MathpixText text=" $\begin{aligned} \left[  \left(\frac{P_{t}}{\overline{P}_{t}^{2}}\right)\right] \end{aligned}$"/>
 
 
-\\\\[
-\\frac{\\mathrm{d} z_{i}^{2}}{2}\\left[w_{+}^{(i)}(\\boldsymbol{z})+w_{-}^{(i)}(\\boldsymbol{z})\\right]
-\\\\]`}/>
-        </div>
-      </div>
+
+
+        {/*<MathpixText text=" $\begin{aligned} 0 &=E_{t}\left[\left(\frac{P_{t}}{\overline{P}_{t}}\right)^{\frac{1}{1-q}} Y_{t}-\frac{1}{1-q}\left(\overline{P}_{t}-P_{t} V_{t}\right)\left(\frac{P_{t}}{\overline{P}_{t}}\right)^{\frac{1}{1-q}-1} Y_{t}\left(\frac{P_{t}}{\overline{P}_{t}^{2}}\right)\right] \\ &+E_{t}\left[R_{t}^{-1}\left\{\left(\frac{P_{t+1}}{\overline{P}_{t}}\right)^{\frac{1}{1-q}} Y_{t+1}-\frac{1}{1-q}\left(\overline{P}_{t}-P_{t+1} V_{t+1}\right)\left(\frac{P_{t+1}}{\overline{P}_{t}}\right)^{\frac{1}{1-q}-1} Y_{t+1}\left(\frac{P_{t+1}}{\overline{P}_{t}^{2}}\right)\right\}\right] \end{aligned}$"/>*/}
+
+
+
+
+        {/*<MathpixText text=" $\begin{array}{l} \underline{A p r}^{B F C}(\widetilde{A}) =\cup\{ \tilde{C}_{i} | \widetilde{C}_{i} \subseteq \widetilde{A}, \widetilde{C}_{i} \in \widehat{\mathbf{C}} \} \\\overline{A p r}^{B F C}(\widetilde{A})=\cap\{\tilde{C}_{i} | \widetilde{C}_{i} \supseteq \widetilde{A}, \widetilde{C}_{i} \in \widehat{\mathbf{C}} \}\end{array}$"/>*/}
+
+          {/*<MathpixText text=" $\begin{aligned} \overline{H^{\prime} S_{2}} & \approx \frac{y_{A}-y_{B}}{\eta} \approx \frac{e\left(\alpha-\frac{\alpha}{n}\right)}{n(\delta+\gamma)-\gamma} \approx \frac{e \alpha \frac{n-1}{n}}{n\left(\alpha-\frac{\alpha}{n}+\frac{y_{B}}{\overline{R}_{2}}\right)-\frac{y_{B}}{\overline R_{2}}} \\ & \approx \frac{\alpha e \frac{n-1}{n}}{n\left(\alpha-\frac{\alpha}{n}\right)+\frac{n-1}{\overline{R}_{2}}\left(\alpha \overline{R}_{1}-e\left(\alpha-\frac{\alpha}{n}\right)\right)} \\ & \approx \frac{e \frac{n-1}{n}}{n-1+(n-1) \frac{\overline{R}_{1}}{\overline{R}_{2}}-e \frac{(n-1)^{2}}{n \overline{R}_{2}}} \end{aligned}$"/>*/}
+
+        {/*<MathpixText text=" \[ \left. \begin{aligned} Q & = A \overline { V } \\ V & = C \sqrt { R I } = f _ { 1 } ( H ) \\ A & = f _ { 2 } ( H ) \\ Q & = f _ { 1 } ( H ) \cdot f _ { 2 } ( H ) = F ( H ) \end{aligned} \right. \]"/>*/}
+
+          {/*<MathpixText text=" \[ \left. \begin{aligned} \underline { P } ( C _ { c } ^ { X } \geq \alpha m _ { x } , C _ { c } ^ { Y } \geq \beta m _ { y } ) & = \underline { P } ( C _ { c } ^ { X } \geq \alpha m _ { x } ) \times \underline { P } ( C _ { c } ^ { Y } \geq \beta m _ { y } ) \\ \overline { P } ( C _ { c } ^ { X } \geq \alpha m _ { x } , C _ { c } ^ { Y } \geq \beta m _ { y } ) & = \overline { P } ( C _ { c } ^ { X } \geq \alpha m _ { x } ) \times \overline { P } ( C _ { c } ^ { Y } \geq \beta m _ { y } ) \end{aligned} \right. \]"/>*/}
+
+        {/*<MathpixText text=" $\begin{aligned}<f, T_{3} g>&=\int_{0}^{1} f(x) \overline{i g^{\prime}(x)} d x=-i \int_{0}^{1} f(x) \overline{g}(x) d x \\ &=-i f(1) \overline{g}(1)+i f(0) \overline{g}(0)+i \int_{0}^{1} f^{\prime}(x) \overline{g(x)} d x \end{aligned}$"/>*/}
+
+          {/*<MathpixText text=" \[ \Delta _{\varepsilon ,\overline{\varepsilon }}^{(0)}= (1-\frac{\sigma ^2 }2) \cdot \frac{m-i\varepsilon\widehat{p}}{2m}\cdot \frac{m-i \overline{\varepsilon p}}{2m}\cdot  ( 1-\sigma _p^2 ) =\Psi_{\varepsilon ,,\overline{\varepsilon }}^{(0)}\cdot \overline{\Psi} _{\varepsilon ,\overline{\varepsilon }}^{(0)}  \]"/>*/}
+
+        {/*<MathpixText text=" \[ \left. \begin{aligned} E _ { \overline { p } } [ s _ { l } ] & = \sum _ { x , y } \tilde { P } ( x , y ) \sum _ { i = 1 } ^ { n + 1 } s _ { l } ( y _ { i } , x , i ) \\ & = \sum _ { x , y } \tilde { P } ( x ) P ( y | x ) \sum _ { i = 1 } ^ { n } s _ { I } ( y _ { i } , x , i ) \exp ( \delta _ { K _ { 1 } + l } T ( x , y ) ) \end{aligned} \right. \]"/>*/}
+
+          {/*<MathpixText text="$\begin{aligned} \tilde{P}_{\mathrm{XPM}}(\omega)=& 2 \gamma_{1} \overline{P}_{1}(0) \tilde{P}_{2}(\omega) \exp \left(\frac{-j \omega L}{v_{g 1}}\right) \\ & \times\left\{\frac{1}{a^{2}+\left(b^{2}+q^{2}\right)}[a \sin (b L)-(b+q) \cos (b L)\right.\\ &+[a \sin (q L)+(b+q) \cos (q L)] \exp (-\alpha L) ] \\ &+\frac{1}{a^{2}+\left(b^{2}-q^{2}\right)}[a \sin (q L)-(b-q) \cos (b L)\\ &+[-a \sin (q L)+(b-q) \cos (q L)] \exp (-\alpha L) ] \} \end{aligned}$"/>*/}
+
+        {/*<MathpixText text=" \(\text {Here, } \Sigma f_{i} u_{i}=85, \Sigma f_{i}=150, g=1000 \)*/}
+{/*\(\begin{aligned}\overline{u}=\frac{\Sigma f_{i} u_{i}}{\Sigma f_{i}} &=\frac{85}{150} \approx 0.56667 \\ \text {Mean } \overline{X} &=A+\overline{u} g \\ &=2500+0.56667 \times 1000 \end{aligned}\)"/>*/}
+
+          {/*<MathpixText text=" $\begin{aligned}\underline P_{G i} & \leqslant P_{G i} \leqslant \overline{P}_{G i} \\ \underline Q_{R i} & \leqslant Q_{R i} \leqslant \overline Q_{R i} \\ \underline V_{i} & \leqslant V_{i} \leqslant \overline{V}_{i} \end{aligned}$"/>*/}
+
+        {/*<MathpixText text=" \[ \overline{\Delta T _ { L }} = \frac { \Delta T_{1} - \Delta T _ { 2 } } { \ln ( \Delta T_{1} / \Delta T _ { 2 } ) } = \frac{90-30}{\ln(3)} = 54.5^{\circ} \mathrm{C} \]"/>*/}
+
+            {/*<MathpixText text=" \[ = ( \mathrm { e } ^ { \alpha } - \mathrm  { e } ^ { - \alpha } ) \sum _ { i = 1 } ^ { N } \overline { w } _ { mi } I ( y _ { i } \neq G ( x _ { i } )) + \mathrm { e } ^ { - \alpha } \sum _ { i = 1 } ^ { N } \overline { w } _ { mi } \]"/>*/}
+
+        {/*<MathpixText text=" \[ = \sum _ { y _ { i } = G _ { m } ( x _ { i } ) } \overline { w } _ { mi } \mathrm { e } ^ { - \alpha } + \sum _ { y _ { i } \neq G _ { m } ( x _ { i } ) } \overline { w } _ { mi } \mathrm { e } ^ { \alpha } \]"/>*/}
+
+            {/*<MathpixText text=" $\begin{aligned} q_{\mathbf{x}}(\mathbf{x}) &=\frac{1}{\mathcal{Z}_{\mathbf{x}}} e^{\sum_{i=1}^{n}\left\langle\ln g(\boldsymbol{\theta})+\ln f\left(\mathbf{x}_{i}, \mathbf{y}_{i}\right)+\boldsymbol{\phi}(\boldsymbol{\theta})^{\top} \mathbf{u}\left(\mathbf{x}_{i}, \mathbf{y}_{i}\right)\right\rangle_{q_{\boldsymbol{\theta}}(\boldsymbol{\theta})}} \\ &=\frac{1}{\mathcal{Z}_{\mathbf{X}}}\left[\prod_{i=1}^{n} f\left(\mathbf{x}_{i}, \mathbf{y}_{i}\right)\right] e^{\sum_{i=1}^{n} \overline{\boldsymbol{\phi}}^{\top} u\left(\mathbf{x}_{i}, \mathbf{y}_{i}\right)} \end{aligned}$"/>*/}
+        {/*<MathpixText text="\[ \left. \begin{aligned} \mathbf { u } & = Y \overline { z } - [ k _ { 1 } \operatorname { sgn } ( s _ { 1 } ) , \cdots , k _ { n } \operatorname { sgn } ( s _ { n } ) ] ^ { T } \\ \overline { z } _ { i } & = - \overline { a } _ { i } \cdot \operatorname { sgn } ( \sum _ { j = 1 } ^ { n } s _ { j } Y _ { j i } ) \quad i = 1 , \cdots , m \end{aligned} \right. \]"/>*/}
+        {/*<MathpixText text="\[ \left. \begin{aligned} r W _ { 1 } ( w ) = & w + \lambda _ { 10 } [ U + U I + s \cdot w - W _ { 1 } ( w ) ] \\ & + \lambda _ { 11 } \int _ { W _ { 1 } ( w ) } ^ { \overline { W } _ { 1 } } \overline { F } _ { 1 } ( x ) d x + \lambda _ { 12 } \int _ { W _ { 1 } ( w ) } ^ { \overline { W } _ { 2 } } \overline { F } _ { 2 } ( x ) d x \end{aligned} \right. \]"/>*/}
+        {/*<MathpixText text="$\begin{aligned} \Delta \overline{S}_{+}^{\mathrm{m}}=& k\left[\left(n_{\mathrm{Na}^{+}}+2 n_{\mathrm{Ca}^{2+}}\right) \ln \left(n_{\mathrm{Na}^{+}}+2 n_{\mathrm{Ca}^{2+}}\right)-\left(n_{\mathrm{Na}}+2 n_{\mathrm{Ca}^{2+}}\right)\right] \\ &-k\left[\left(n_{\mathrm{Na}^{+}} \ln n_{\mathrm{Na}^{+}}-n_{\mathrm{Na}^{+}}\right)+\left(2 n_{\mathrm{Ca}^{2+}} \ln 2 n_{\mathrm{Ca}^{2+}}-2 n_{\mathrm{Ca}^{2+}}\right)\right] \\=& k\left[n_{\mathrm{Na}^{+}} \ln \left(n_{\mathrm{Na}^{+}}+2 n_{\mathrm{Ca}^{2+}}\right)+2 n_{\mathrm{Ca}^{2+}} \ln \left(n_{\mathrm{Na}^{2+}}+2 n_{\mathrm{Ca}^{2+}}\right)-n_{\mathrm{Na}^{+}}-2 n_{\mathrm{Ca}^{2+}}\right.\end{aligned}$"/>*/}
+        {/*<MathpixText text=" $\begin{aligned} E\left(\overline{y}_{s t}\right)&=\frac{1}{N} \sum_{i=1}^{k} N_{i} E\left(\overline{y}_{i}\right) \\ =&\frac{1}{N} \sum_{i=1}^{k} N_{i} \overline{Y}_{i} \\ =&\overline{Y} \end{aligned}$"/>*/}
+        {/*<MathpixText text="$\begin{aligned} \text { subject to : }  & \mathbf{K U}=\mathbf{F} \\ & V \leq \overline{V} \\ & 0.025 \leq x_{1}^{i}, x_{2}^{i} \leq 0.25, \quad i=1, \ldots, n \\ & 0.035 \leq x_{3}^{i}, x_{4}^{i} \leq 0.35, \quad i=1, \ldots, n \end{aligned}$"/>*/}
+        {/*<MathpixText text="\[ \left. \begin{aligned} P ( \xi = 18000 ) =& P ( A _ { 1 } A _ { 2 } \overline { A } _ { 3 } ) + P ( A _ { 1 } \overline { A } _ { 2 } A _ { 3 } ) + P ( \overline { A } _ { 1 } A _ { 2 } A _ { 3 } ) \\ =& P ( A _ { 1 } ) P ( A _ { 2 } ) P ( \overline { A } _ { 3 } ) + P ( A _ { 1 } ) P ( \overline { A } _ { 2 } ) P ( A _ { 3 } ) + P ( \overline { A } _ { 1 } ) P ( A _ { 2 } ) \\ &  P ( A _ { 3 } ) \\ =& \frac { 1 } { 9 } \times \frac { 1 } { 10 } \times \frac { 10 } { 11 } + \frac { 1 } { 9 } \times \frac { 9 } { 10 } \times \frac { 1 } { 11 } + \frac { 8 } { 9 } \times \frac { 1 } { 10 } \times \frac { 1 } { 11 } \\ =& \frac { 27 } { 990 } = \frac { 3 } { 110 } \end{aligned} \right. \]"/>*/}
+        {/*<MathpixText text="$\begin{aligned} &=\sum_{i=1}^{n} m_{i}\left(\mathbf{r}_{i}-\mathbf{r}_{A}\right) \times \mathbf{v}_{A}+\mathbf{H}_{A}^{\prime} \\ &=m\left(\overline{\mathbf{r}}-\mathbf{r}_{A}\right) \times \mathbf{v}_{A}+\mathbf{H}_{A}^{\prime} \\ \mathbf{H}_{A}&=\mathbf{H}_{A}^{\prime} \text { if, and only if, } \qquad m\left(\mathbf{r}-\mathbf{r}_{A}\right) \times \mathbf{v}_{A}=0 \end{aligned}$"/>*/}
+        {/*<MathpixText text="$\left\{\begin{aligned} \hat{\gamma}=&\left[\sum_{i=1}^{k} \sum_{j=1}^{m_{i}} x_{i j}\left(x_{i j}-\overline{x}_{i}\right)\right]^{-1}\left[\sum_{i=1}^{k} \sum_{j=1}^{m_{i}} x_{i j}\left(y_{i j}-\overline{y}_{i}\right)\right] \\ \hat{\mu}_{i} &=\frac{\sum_{j=1}^{m_i}\left(y_{i j}-\hat{\gamma} x_{i j}\right)}{m_{i}} \end{aligned}\right.$"/>*/}
+        {/*<MathpixText text="$\begin{aligned} &=\frac{1}{2}\left(\sum_{m \in A_{i}, n \notin A_{i}} w_{m n} \frac{1}{\left|A_{i}\right|}+\sum_{m \notin A_{i}, n \in A_{i}} w_{m n} \frac{1}{\left|A_{i}\right|}\right.\\ &=\frac{1}{2}\left(\operatorname{cut}\left(A_{i}, \overline{A}_{i}\right) \frac{1}{\left|A_{i}\right|}+\operatorname{cut}\left(\overline{A}_{i}, A_{i}\right) \frac{1}{\left|A_{i}\right|}\right) \end{aligned}$"/>*/}
+        {/*<MathpixText text="$\begin{array}{l} \mathbf{P} =\overline{\overline{\chi}}_{\mathrm{ee}} \cdot\langle\mathbf{E}\rangle+\overline{\overline{\chi}}_{\mathrm{em}} \cdot\langle\mathbf{H}\rangle \\ \mathbf{M} =\overline{\overline{\chi}}_{\mathrm{me}} \cdot\langle\mathbf{E}\rangle+\overline{\overline{\chi}}_{\mathrm{mm}} \cdot\langle\mathbf{H}\rangle \end{array}$"/>*/}
+        {/*<MathpixText text=" $\pi_{i}\left(l_{i}, l_{j}\right)=\left\{\begin{array}{c l}(\overline{p}-c)\left(l_{i}+l_{j}\right) / 2 & \text { if } l_{i}<l_{j} \\(\overline{p}-c) / 2 & \text { if } l_{i}=l_{j} \\(\overline{p}-c)\left[1-\left(l_{i}+l_{j}\right) / 2\right] & \text { if } l_{i}>l_{j} \end{array}\right.$"/>*/}
+        {/*<MathpixText text="\[ \left\{ \begin{array} { l } { \overline { x y } + \overline { y x } = 9 } \\ { \overline { x y } - \overline { y x } = 45 } \end{array} \right. \]"/>*/}
+        {/*<MathpixText text="\[ ( \delta f _ { z } - A _ { 0 } ) = e ^ { - p \cdot \vec{\mathrm{V}} } \delta \overline { v } _ { z } \]"/>*/}
+        {/*<MathpixText text="\[\alpha = \overline { q } _ { 2 } \]"/>*/}
+        {/*<MathpixText text="\[ \frac { 1 } { \sigma \sqrt { 2 \pi } } \times \exp ( \frac { ( y - \overline { y } ) ^ { T } \Sigma ^ { - 1 } ( y - \overline { y } ) } { \sigma ^ { 2 } } ) \]"/>*/}
+      </MathpixLoader>
     );
   }
 }
